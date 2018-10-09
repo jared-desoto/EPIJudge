23dc098079379be0b91883ad62eeed27061bf2be
Jared DeSoto <lozoot123@gmail.com>
2018-10-09 16:14:46 -0500

    String->Integer / Integer->String solution

--- epi_judge_python/string_integer_interconversion.py
+++ epi_judge_python/string_integer_interconversion.py
@@ -3,13 +3,29 @@ from test_framework.test_failure import TestFailure
 
 
 def int_to_string(x):
-    # TODO - you fill in here.
-    return ''
+    is_negative = False
+    if x < 0:
+        is_negative = True
+        x *= -1
+
+    char_arr = []
+    while True:
+        char_arr.append(chr(ord('0') + x % 10))  # ORD grabs the ASCII value of 0 (48). We then add to that our value
+        x //= 10
+        if x == 0: break
+
+    return ('-' if is_negative else '') + ''.join(reversed(char_arr))
 
 
 def string_to_int(s):
-    # TODO - you fill in here.
-    return 0
+
+    decimal_place, result, is_negative = len(s) - 1, 0, False
+    for char in s:
+        if char == '-': is_negative = True
+        else: result += (ord(char) - ord('0'))*pow(10, decimal_place)
+        decimal_place -= 1
+    if is_negative: result = -result
+    return result
 
 
 def wrapper(x, s):
