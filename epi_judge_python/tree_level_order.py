de03b1357863ec6e39e6699974fbc10242e8d11d
Jared DeSoto <lozoot123@gmail.com>
2018-10-12 22:55:48 -0500

    Tree Level Order solution

--- epi_judge_python/tree_level_order.py
+++ epi_judge_python/tree_level_order.py
@@ -1,10 +1,20 @@
 from test_framework import generic_test
+from Lib import collections
 
 
 def binary_tree_depth_order(tree):
-    # TODO - you fill in here.
-    return []
-
+    result, cur_que = [], collections.deque([tree])
+    while cur_que:
+        cur_arr, next_que = [], collections.deque([])
+        while cur_que:
+            current = cur_que.popleft()
+            if current:
+                cur_arr.append(current.data)
+                next_que.append(current.left); next_que.append(current.right)
+        if cur_arr:
+            result.append(cur_arr)
+        cur_que = next_que
+    return result
 
 if __name__ == '__main__':
     exit(
