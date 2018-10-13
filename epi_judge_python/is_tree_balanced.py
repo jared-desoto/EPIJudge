cc458c5bf60722a323ece0d796ebff00eba278f5
Jared DeSoto <lozoot123@gmail.com>
2018-10-13 16:24:31 -0500

    Tree is height balanced solution

--- epi_judge_python/is_tree_balanced.py
+++ epi_judge_python/is_tree_balanced.py
@@ -1,9 +1,28 @@
 from test_framework import generic_test
+from collections import namedtuple
 
 
 def is_balanced_binary_tree(tree):
-    # TODO - you fill in here.
-    return True
+    height_isbalanced = namedtuple('HeightBalanced', ('height', 'isbalanced'))
+
+    def calculateBalance(node):
+        if not node:
+            return height_isbalanced(-1, True)
+
+        left = calculateBalance(node.left)
+        if not left.isbalanced:
+            return height_isbalanced(left.height, left.isbalanced)
+        right = calculateBalance(node.right)
+        if not right.isbalanced:
+            return height_isbalanced(right.height, right.isbalanced)
+
+        max_value = max(left.height, right.height)+1
+        is_balanced = False
+        if abs(right.height - left.height) <= 1:
+            is_balanced = True
+
+        return height_isbalanced(max_value, is_balanced)
+    return calculateBalance(tree).isbalanced
 
 
 if __name__ == '__main__':
