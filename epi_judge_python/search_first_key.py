bcdeac04f0de9b0a86092b33224016e9418b200b
Jared DeSoto <lozoot123@gmail.com>
2018-10-17 13:06:11 -0500

    Search first key solution

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -1,10 +1,22 @@
 from test_framework import generic_test
+from Lib import bisect
 
 
 def search_first_of_k(A, k):
-    # TODO - you fill in here.
-    return 0
 
+    left, right, result = 0, len(A) - 1, -1
+    while left <= right:
+        mid = (left + right) // 2
+        if A[mid] > k:
+            right = mid - 1
+            mid = (left + right) // 2
+        elif A[mid] < k:
+            left = mid + 1
+            mid = (left + right) //2
+        else:  # A[mid] == k:
+            result = mid
+            right = mid - 1
+    return result
 
 if __name__ == '__main__':
     exit(
