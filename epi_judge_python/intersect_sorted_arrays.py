734bba820239fd7362c24e4b76657e94b741c093
Jared DeSoto <lozoot123@gmail.com>
2018-10-18 13:45:45 -0500

    10.1 Intersection of two Sorted Arrays Solution + 11.1 Test if a Binary Tree satisfies the BST property

--- epi_judge_python/intersect_sorted_arrays.py
+++ epi_judge_python/intersect_sorted_arrays.py
@@ -1,10 +1,30 @@
 from test_framework import generic_test
 
+# Initial algorithm: O(N+M) time complexity O(N) space complexity
+def intersect_two_sorted_arrays_extra_space(A, B):
+    a_dict = {}
+    result = []
+    for i in A:
+        if i not in a_dict:
+            a_dict[i] = 1
+    for j in B:
+        if j in a_dict and (len(result) == 0 or j != result[-1]):
+            result.append(j)
+    return result
 
+# For Better Space Complexity: O(1)
 def intersect_two_sorted_arrays(A, B):
-    # TODO - you fill in here.
-    return []
-
+    result, i, j = [], 0, 0
+    while i < len(A) and j < len(B):
+        if A[i] == B[j]:
+            if i == 0 or A[i] != A[i - 1]:
+                result.append(A[i])
+            i, j = i + 1, j + 1
+        elif A[i] < B[j]:
+            i += 1
+        else:  # B[i] < A[i]:
+            j += 1
+    return result
 
 if __name__ == '__main__':
     exit(
--- epi_judge_python/is_tree_a_bst.py
+++ epi_judge_python/is_tree_a_bst.py
@@ -1,10 +1,34 @@
 from test_framework import generic_test
+from Lib import collections
 
-
+# recursive solution utilizing property that child must exist in a range established by parent
+# O(N) runtime solution with O(h) space (From recursive calls)
 def is_binary_tree_bst(tree, low_range=float('-inf'), high_range=float('inf')):
-    # TODO - you fill in here.
-    return True
 
+    if not tree:
+        return True
+    if low_range <= tree.data <= high_range :
+        return (is_binary_tree_bst(tree.left, low_range, tree.data) and # left child
+                is_binary_tree_bst(tree.right, tree.data, high_range))  # right child
+    else:
+        return False
+
+# iterative solution by in order traversal and verifying keys are in ascending order
+# O(N) runtime with O(N) space
+def is_binary_tree_bst_iterative(tree):
+    QueueEntry = collections.namedtuple('QueueEntry', ('node', 'lower', 'upper'))
+    bfs_queue = collections.deque(
+        [QueueEntry(tree, float('-inf'), float('inf'))])
+    while bfs_queue:
+        front = bfs_queue.popleft()
+        if front.node:
+            if not front.lower <= front.node.data <= front.upper:
+                return False
+            bfs_queue += [
+                QueueEntry(front.node.left, front.lower, front.node.data),
+                QueueEntry(front.node.right, front.node.data, front.upper)
+                ]
+    return True
 
 if __name__ == '__main__':
     exit(
